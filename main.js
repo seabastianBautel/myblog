eb2bfe24201d35d379cd36838bfeb30d4446624c
seabastianBautel <sebastianarielbautel@gmail.com>
2023-05-08 21:18:02 -0300

    al scrollear el navbar desaparece

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let ubicacionPrincipal  = window.pageYOffset;
 window.onscroll = function() {
     let Desplazamiento_Actual = window.pageYOffset;
-    if(ubicacionPrincipal = Desplazamiento_Actual){
-        document.getElementById('navbar').style.display= '0';
+    if(ubicacionPrincipal <= Desplazamiento_Actual){
+        document.getElementById('navbar').style.display="none";
+        document.getElementById('navbar').style.paddingBottom="0";
+        document.getElementById('navbar').style.paddingTop="0";
+        console.info("entro");
     }
     else{
-        document.getElementById('navbar').style.top = '-100px';
+        document.getElementById('navbar').style.display="flex";
+        document.getElementById('navbar').style.paddingBottom="10px";
+        document.getElementById('navbar').style.paddingTop="6px";
+        console.info("salio")
     }
     ubicacionPrincipal = Desplazamiento_Actual;
 }
