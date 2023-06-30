8347bee8f0c42a6310dbcdc877e9e79f14f36e44
seabastianBautel <sebastianarielbautel@gmail.com>
2023-06-29 23:19:20 -0300

    slider columns incons

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ let ubicacionPrincipal  = window.pageYOffset;
 window.onscroll = function() {
     let Desplazamiento_Actual = window.pageYOffset;
     if(ubicacionPrincipal <= Desplazamiento_Actual){
-        document.getElementById('navbar').style.display="none";
-        document.getElementById('navbar').style.paddingBottom="0";
-        document.getElementById('navbar').style.paddingTop="0";
+        document.getElementById('navbar-logo').style.display="none";
+        document.getElementById('navbar-logo').style.paddingBottom="0";
+        document.getElementById('navbar-logo').style.paddingTop="0";
         console.info("entro");
     }
     else{
-        document.getElementById('navbar').style.display="flex";
-        document.getElementById('navbar').style.paddingBottom="10px";
-        document.getElementById('navbar').style.paddingTop="6px";
+        document.getElementById('navbar-logo').style.display="flex";
+        document.getElementById('navbar-logo').style.paddingBottom="10px";
+        document.getElementById('navbar-logo').style.paddingTop="6px";
         console.info("salio")
     }
     ubicacionPrincipal = Desplazamiento_Actual;
